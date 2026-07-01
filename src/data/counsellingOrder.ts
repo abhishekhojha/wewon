9861ef06827107a2ae20a0e0c5026d550491e5a2
yash-mandlekar <ymandlekar9826@gmail.com>
2026-07-01 15:20:22 +0530

    refactor: reorder counselling programs and add new entries for IISER and CSAS DU

--- src/data/counsellingOrder.ts
+++ src/data/counsellingOrder.ts
@@ -1,19 +1,20 @@
 export const COUNSELLING_SLUG_ORDER = [
-
-  "josaa-csab-combo-program",
-  "josaa-counselling-support",
-  "csab-counselling-support",
-  "uptac-counselling-full-program",
-  "uptac-hbtu-mmmut-full-support-combo",
-  "ggsipu-full-counselling-program",
-  "mpdte-full-counselling-support",
-  "wbjee-full-counselling-program",
-  "all-india-full-counselling-support",
-  "aktu-ggsipu-full-combo-support",
-  "hbtu-mmmut-combo",
-  "jac-delhi-full-counselling-program",
-  "josaa-csab-jac-delhi-combo-counselling-program-2026",
-  "ggsipu-jac-delhi-jac-chandigarh-full-counselling-program",
-  "ggsipu-jac-delhi-full-counselling-program",
-  "jac-chandigarh-full-support",
+  "csab-counselling-support",                                    // 1. CSAB
+  "uptac-counselling-full-program",                              // 2. UPTAC
+  "iisercounselling2026",                                        // 3. IISER
+  "wbjee-full-counselling-program",                               // 4. WBJEE
+  "du-csas-counselling-2026",                                    // 5. CSAS DU
+  "mpdte-full-counselling-support",                               // 6. MPDTE
+  "ggsipu-full-counselling-program",                              // 7. GGSIPU
+  "uptac-hbtu-mmmut-full-support-combo",                          // 8. UPTAC HBTU MMMUT
+  "all-india-full-counselling-support",                           // 9. ALL INDIA
+  "josaa-csab-combo-program",                                     // 10. JOSAA CSAB
+  "josaa-counselling-support",                                    // 11. JOSAA
+  "josaa-csab-jac-delhi-combo-counselling-program-2026",          // 12. JOSAA CSAB JAC
+  "jac-delhi-full-counselling-program",                           // 13. JAC DELHI
+  "aktu-ggsipu-full-combo-support",                               // 14. UPTAC IPU
+  "hbtu-mmmut-combo",                                             // 15. HBTU MMMUT
+  "ggsipu-jac-delhi-jac-chandigarh-full-counselling-program",     // 16. JAC DELHI CHANDI IPU
+  "ggsipu-jac-delhi-full-counselling-program",                    // 17. JAC IPU
+  "jac-chandigarh-full-support",                                  // 18. JAC CHANDIGARH
 ];
